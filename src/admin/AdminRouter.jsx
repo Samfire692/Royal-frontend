947ed1bcec76a5c3bf0babf00a67b6f60e2d20ff
unknown <akinsolasamuel372@gmail.com>
2026-04-02 17:33:43 +0100

    feat:updated

--- src/admin/AdminRouter.jsx
+++ src/admin/AdminRouter.jsx
@@ -17,9 +17,10 @@ export const AdminRouter = () => {
   const [navbar, setNavbar] = useState(false)
 
   return (
-   <Routes>
-        <Route element={
-          <div className='text-black'>
+    <Routes>
+      {/* PARENT ROUTE: This handles the Layout and Sidebar */}
+      <Route path="/admin" element={
+        <div className='text-black'>
 
           <div className='lg:block hidden'>
             <AdminNavbar />
@@ -27,53 +28,49 @@ export const AdminRouter = () => {
 
           <div>
             {navbar && (
-             <div className='flex'>
-              <AdminNavbar />
-              <button className='text-white border-0 rounded-xl w-9 h-9 p-1.5 absolute text-2xl right-22 md:left-90 top-2 bg-red-500' onClick={()=> setNavbar(false)}><FaTimes/></button>
-             </div>
-          )}
+              <div className='flex'>
+                <AdminNavbar />
+                <button className='text-white border-0 rounded-xl w-9 h-9 p-1.5 absolute text-2xl right-22 md:left-90 top-2 bg-red-500' onClick={() => setNavbar(false)}><FaTimes /></button>
+              </div>
+            )}
           </div>
-           
-          <div className='p-2 flex w-screen justify-between border border-b-slate-400 border-t-0 border-r-0 border-l-0'>
-           <div>
-             <button className='border rounded-xl text-xl text-black p-1.5 lg:hidden mt-3' onClick={()=> setNavbar(true)}><FaBars/></button>
-           </div>
 
-           <div>
-            
-           </div>
+          <div className='p-2 flex w-screen justify-between border border-b-slate-400 border-t-0 border-r-0 border-l-0'>
+            <div>
+              <button className='border rounded-xl text-xl text-black p-1.5 lg:hidden mt-3' onClick={() => setNavbar(true)}><FaBars /></button>
+            </div>
+
+            <div>
+              {/* Space for center content if needed */}
+            </div>
 
             <div className='flex justify-around gap-1.5 px-2'>
-              <Search/>
-              <Link>
-               <button className='bg-slate-300 p-2.5 rounded-full mt-2 h-11 w-11'><FaBell className='text-2xl'/></button>
+              <Search />
+              <Link to="#">
+                <button className='bg-slate-300 p-2.5 rounded-full mt-2 h-11 w-11'><FaBell className='text-2xl' /></button>
               </Link>
-              {/* <img src={profilepic} alt="dummy profile pic" className='w-12 h-12 my-0.5 rounded-full shadow shadow-slate-400'/> */}
             </div>
 
           </div>
-           
-           <div className='px-2 lg:mx-20 pt-2'>
-            <Outlet/>
-           </div>
+
+          <div className='px-2 lg:mx-20 pt-2'>
+            {/* CHILD PAGES LOAD HERE */}
+            <Outlet />
           </div>
-        }>
-        <Route path='/admin/' element={<Login/>}/>
+        </div>
+      }>
+        {/* CHILD ROUTES: No need for /admin/ prefix here because they are nested */}
+        <Route index element={<Login />} />
+        <Route path='dashboard' element={<Dashboard />} />
+        <Route path='profile' element={<AdminProfile />} />
+        <Route path='sitemanagement' element={<AdminSitemanagement />} />
+        <Route path='promotion' element={<Promotion />} />
+        <Route path='announcementboard' element={<AdminAnnouncementBoard />} />
+        <Route path='signin' element={<AdminSignin />} />
+      </Route>
 
-        <Route path='/admin/dashboard' element={<Dashboard/>}/>
-
-        <Route path='/admin/profile' element={<AdminProfile />} />
-
-        <Route path='/admin/sitemanagement' element={<AdminSitemanagement />} />
-
-        <Route path='/admin/promotion' element={<Promotion />} />
-
-        <Route path='/admin/announcementboard' element={<AdminAnnouncementBoard />} />
-
-        <Route path='/admin/signin' element={<AdminSignin />} />
-        </Route>
-
-         <Route path='/admin/signup' element={<Signup/>} />
-   </Routes>
+      {/* OUTSIDE THE LAYOUT: Signup page */}
+      <Route path='/admin/signup' element={<Signup />} />
+    </Routes>
   )
 }
