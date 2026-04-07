b7aa183b54affb184d4f0f8d2333680d68a780a3
unknown <akinsolasamuel372@gmail.com>
2026-04-07 06:40:45 +0100

    updated:package Json and viteConifg

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Ensures paths work correctly on Vercel
   server: {
     host: true
+  },
+  build: {
+    outDir: 'dist', // This is the folder Vercel is looking for!
+    emptyOutDir: true,
   }
 })
